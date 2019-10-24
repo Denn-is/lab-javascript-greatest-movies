083e4ec563a49bdcfd691edf10f32b9ce006e0bc
Dennis <deheeu@gmail.com>
2019-10-24 15:46:52 +0200

    iteration 1 & 2 complete

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,44 @@
+
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals
+function calculateAverageMovieRate (arr) {
+    const sumOfRating = arr.reduce((accumulator, value, index, originalArray) => {
+     return accumulator + arr[index].rate;
+ }, 0);
+ //  return (parseFloat(Math.round((sumOfRating/arr.length) * 100) / 100).toFixed(2))
+    return Number((sumOfRating / arr.length).toFixed(2));
+ };
+ 
+ calculateAverageMovieRate(MOVIES)
 
 // Iteration 2: Drama movies - Get the average of Drama Movies
+function calculateAverageDramaRate (arr) {
+    const dramaArray = arr.filter((movie) => {
+      return movie.genre.includes("Drama")});
+    if (Number(dramaArray) === 0) {
+      return 0
+    } else {
+      return calculateAverageMovieRate(dramaArray);
+    }
+};
+
+calculateAverageDramaRate(MOVIES)
+
+/*
+function calculateAverageDramaRate (arr) {
+    const dramaArray = MOVIES.filter((movie) => {
+      return movie.genre.includes("Drama")});
+    const sumOfRating = arr.reduce((accumulator, value, index, originalArray) => {
+      return accumulator + arr[index].rate;
+    }, 0);
+    return Number((sumOfRating / arr.length).toFixed(2));
+};
+   
+calculateAverageDramaRate(MOVIES)
+*/
+
+
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
