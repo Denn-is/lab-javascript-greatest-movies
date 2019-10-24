c68a329b274de2a5445337307d8656e551d49f6d
Dennis <deheeu@gmail.com>
2019-10-24 22:02:35 +0200

    1-4 done; 5 test failing, but correct results; 6 not managing to isolate the minutes number for addition in elegant way; 7 not stated

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -46,8 +46,43 @@ orderByYear(MOVIES);
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
+function countSpielbergDramaMovies(arr) {
+    const spielbergArray = arr.filter((movie) => {
+        return movie.director.includes("Spielberg")});
+        const dramaArray = spielbergArray.filter((movie) => {
+            return movie.genre.includes("Drama")});
+    return dramaArray.length;
+}
+
+countSpielbergDramaMovies(MOVIES)
+
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically (arr) {
+    let allAlphabeticallyOrdered = [ ...arr ];
+    let topTwenty = [];
+    allAlphabeticallyOrdered.sort((a, b) => {
+        if (a.title.toLowerCase() > b.title.toLowerCase()) {
+            return 1;
+        } else if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }); for (i = 0; i < 20 ; i++) {
+        topTwenty.push(allAlphabeticallyOrdered[i].title)
+    };  return topTwenty;
+};
+
+orderAlphabetically(MOVIES)
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+console.log(MOVIES[21].duration.substring(0,1)*60);
+
+console.log(Number(MOVIES[21].duration.substring(2,5)));
+
+console.log(Number(MOVIES[21].duration.substring(0,1)*60) + Number(MOVIES[21].duration.substring(2,5)))
+
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
