69c10a6ab02c5e3ca54fe861e8167867a523de00
Dennis <deheeu@gmail.com>
2019-11-03 02:38:09 +0100

    refactored after starting from scratch, iteration 5 & with bugs, iteration 7 not yet done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,88 +1,123 @@
-
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals
-function calculateAverageMovieRate (arr) {
-    const sumOfRating = arr.reduce((accumulator, value, index, originalArray) => {
-     return accumulator + arr[index].rate;
- }, 0);
- //  return (parseFloat(Math.round((sumOfRating/arr.length) * 100) / 100).toFixed(2))
-    return Number((sumOfRating / arr.length).toFixed(2));
- };
- 
- calculateAverageMovieRate(MOVIES)
-
-// Iteration 2: Drama movies - Get the average of Drama Movies
-function calculateAverageDramaRate (arr) {
-    const dramaArray = arr.filter((movie) => {
-      return movie.genre.includes("Drama")});
-    if (dramaArray.length === 0) {
-      return 0
-    } else {
-      return calculateAverageMovieRate(dramaArray);
-    }
-};
-
-calculateAverageDramaRate(MOVIES)
-
-// Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
-
-function orderByYear (arr) {
-    const byYearArray = [ ...arr ];
-    byYearArray.sort((a, b) => {
-        if (a.year > b.year) {
-            return 1;
-        } else if (a.year < b.year) {
-            return -1;
-        } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
-            return 1;
-        } else if (a.title.toLowerCase() < b.title.toLowerCase()) { return -1;}
-    }); return byYearArray;
+function calculateAverageMovieRate(movies) {
+	if (!movies.length) {
+		return 0;
+	} else {
+		const sum = movies.reduce((sum, movie) => {
+			return sum + movie.rate;
+		}, 0);
+		return parseFloat(sum / movies.length);
+	}
 }
 
-orderByYear(MOVIES);
-
-
-
-// Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-
-function countSpielbergDramaMovies(arr) {
-    const spielbergArray = arr.filter((movie) => {
-        return movie.director.includes("Spielberg")});
-        const dramaArray = spielbergArray.filter((movie) => {
-            return movie.genre.includes("Drama")});
-    return dramaArray.length;
+function calculateAverageDramaRate(movies) {
+	const dramaMovies = movies.filter(movie => movie.genre.includes("Drama"));
+	if (!dramaMovies.length) {
+		return 0;
+	} else {
+		return calculateAverageMovieRate(dramaMovies);
+	}
 }
 
-countSpielbergDramaMovies(MOVIES)
+function orderByYear(movies) {
+	const moviesSortedByYear = [...movies];
+	moviesSortedByYear.sort((a, b) => {
+		if (a.year > b.year) {
+			return 1;
+		} else if (a.year < b.year) {
+			return -1;
+		} else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+			return 1;
+		} else if (a.title.toLowerCase() < b.title.toLowerCase()) {
+			return -1;
+		}
+	});
+	return moviesSortedByYear;
+}
 
-// Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
+function countSpielbergDramaMovies(movies) {
+	const spielbergDramaMovies = movies.filter(
+		movie =>
+			movie.genre.includes("Drama") && movie.director === "Steven Spielberg"
+	);
+	if (!spielbergDramaMovies.length) {
+		return 0;
+	} else {
+		return spielbergDramaMovies.length;
+	}
+}
 
-function orderAlphabetically (arr) {
-    let allAlphabeticallyOrdered = [ ...arr ];
-    let topTwenty = [];
-    allAlphabeticallyOrdered.sort((a, b) => {
-        if (a.title.toLowerCase() > b.title.toLowerCase()) {
-            return 1;
-        } else if (a.title.toLowerCase() < b.title.toLowerCase()) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }); for (i = 0; i < 20 ; i++) {
-        topTwenty.push(allAlphabeticallyOrdered[i].title)
-    };  return topTwenty;
-};
+function orderAlphabetically(movies) {
+	return movies
+		.map(movie => movie.title)
+		.sort((a, b) => {
+			const firstMovieTitle = a.toLowerCase();
+			const secondMovieTitle = b.toLowerCase();
+			if (firstMovieTitle > secondMovieTitle) {
+				return 1;
+			} else if (firstMovieTitle < secondMovieTitle) {
+				return -1;
+			} else {
+				return 0;
+			}
+		})
+		.slice(0, 20);
+}
 
-orderAlphabetically(MOVIES)
+// function orderAlphabetically(arr) {
+// 	let allAlphabeticallyOrdered = [...arr];
+// 	let topTwenty = [];
+// 	allAlphabeticallyOrdered.sort((a, b) => {
+// 		if (a.title.toLowerCase() > b.title.toLowerCase()) {
+// 			return 1;
+// 		} else if (a.title.toLowerCase() < b.title.toLowerCase()) {
+// 			return -1;
+// 		} else {
+// 			return 0;
+// 		}
+// 	});
+// 	for (i = 0; i < 20; i++) {
+// 		topTwenty.push(allAlphabeticallyOrdered[i].title);
+// 	}
+// 	return topTwenty;
+// }
 
-// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies) {
+	return movies.map(movie => {
+		const duration = movie.duration.split(" ");
+		let minutes = 0;
+		for (let time of duration) {
+			if (time.includes("h")) {
+				minutes += parseInt(time) * 60;
+			} else {
+				minutes += parseInt(time);
+			}
+		}
+		return {
+			...movie,
+			duration: minutes
+		};
+	});
+}
 
-console.log(MOVIES[21].duration.substring(0,1)*60);
+// function turnAnyHoursToMinutes(time) {
+// 	const splitTime = time.split(" ");
+// 	let minutes = 0;
+// 	if (splitTime.includes("h")) {
+// 		return (minutes = parseInt(splitTime[0]) * 60 + parseInt(splitTime[1]));
+// 	} else {
+// 		return (minutes = parseInt(splitTime[1]));
+// 	}
+// }
 
-console.log(Number(MOVIES[21].duration.substring(2,5)));
-
-console.log(Number(MOVIES[21].duration.substring(0,1)*60) + Number(MOVIES[21].duration.substring(2,5)))
-
+// function turnHoursToMinutes(movies) {
+// 	return movies.map(movie => {
+// 		return {
+// 			...movie,
+// 			duration: turnAnyHoursToMinutes(movies.duration)
+// 		};
+// 	});
+// }
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
